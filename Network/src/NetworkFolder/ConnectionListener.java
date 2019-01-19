73694013e043fa2a2367b7ad6d3c9a397b669f33
Vladislav <vasilievvladislav12@gmail.com>
2019-01-19 20:15:45 +0300

    First Commit

--- Network/src/NetworkFolder/ConnectionListener.java
+++ Network/src/NetworkFolder/ConnectionListener.java
@@ -1,12 +1,10 @@
 package NetworkFolder;
 
-//14.Создаём интерфейс для работы с событиями (задаём варианты поведений), для прослушивания во входящем потоке.
 public interface ConnectionListener {
 
-    //15.Создаём методы для работы с соединением, передавая в качестве параметра сущность класса NetworkTCP.
-    void onConnectionReady(NetworkTCP networkTCP);//16.Создаём метод для подключённого соединения.
-    void onReceiveString(NetworkTCP networkTCP, String value);//17.Создаём метод для принятия строки, кроме сущности NetworkTCP, добавляем String value если хотим увидеть конкретно принятую строку.
-    void onDisconnect(NetworkTCP networkTCP);//18.Создаём метод для разорванного соединения.
-    void onException(NetworkTCP networkTCP, Exception e);//19.Создаём метод для исключений, отловленных в соединениях.
+    void onConnectionReady(NetworkTCP networkTCP);
+    void onReceiveString(NetworkTCP networkTCP, String value);
+    void onDisconnect(NetworkTCP networkTCP);
+    void onException(NetworkTCP networkTCP, Exception e);
 
 }
